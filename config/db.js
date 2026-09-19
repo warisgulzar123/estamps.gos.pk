a625132b0271be96c681989145d0c7d75acb4e3c
warisgulzar123 <warisaligulzar123@gmail.com>
2026-09-19 22:16:56 +0500

    Add DNS server resolution in db.js to prevent Atlas SRV timeout

--- config/db.js
+++ config/db.js
@@ -1,3 +1,8 @@
+const dns = require('dns');
+try {
+    dns.setServers(['8.8.8.8', '1.1.1.1', '8.8.4.4']);
+} catch (e) {}
+
 const mongoose = require('mongoose');
 const { MongoServerError } = require('mongodb');
 
--- test-e2e-integration.js
+++ test-e2e-integration.js
@@ -4,6 +4,8 @@
  */
 
 require('dotenv').config();
+const dns = require('dns');
+try { dns.setServers(['8.8.8.8', '1.1.1.1', '8.8.4.4']); } catch(e) {}
 const express = require('express');
 const http = require('http');
 const axios = require('axios');
